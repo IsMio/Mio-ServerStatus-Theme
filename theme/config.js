4f62e945496f749be50fb46eec2468c8c5d9af01
Mio-Feng <69396063+Mio-Feng@users.noreply.github.com>
2022-08-28 16:19:19 +0800

    Add files via upload

---
+++ theme/config.js
@@ -0,0 +1,6 @@
+window.__PRE_CONFIG__ = {
+  header: 'Mio\'s Servers Status',
+  //#endregionsubHeader: 'Servers\' Probes Set up with ServerStatus',
+  interval: 1.5,
+  footer: '<p>此页面由 <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a>驱动</p>'
+};
